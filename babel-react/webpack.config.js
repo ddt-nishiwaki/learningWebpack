a58dfc6a58d6a98afbcad11c32a5c25e53a57698
SeijiNishiwaki <nishiwakiat.wjnisi@gmail.com>
2018-09-06 17:52:12 +0900

    【課題】ECMAScriptの利用(React併用)
    書籍の例がReactとの組み合わせになっているので次のパッケージを利用しています
    NOTE: babel-loader のドキュメント通りの設定を行うと エラーになるため babel-core を入れています
    参考:
        https://github.com/babel/babel-loader
    
    react
    react-dom
    babel-loader
    babel-core
    @babel/core
    @babel/preset-env
    @babel/preset-react

---
+++ babel-react/webpack.config.js
@@ -0,0 +1,22 @@
+// webpack setting
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: `${__dirname}/dist`,
+    filename: 'main.js'
+  },
+  devServer: {
+    contentBase: './dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+        // babelの設定は .babelrc に切り分けています
+      }
+    ]
+  }
+};
